46ae36a895493542900b181012a43f0e5ee693de
이하성 <tedsoftj1123@gmail.com>
2024-04-15 12:03:42 +0900

    setting: (#main) init springboot project

---
+++ src/main/kotlin/kr/dsm/payedin/PayedInApplication.kt
@@ -0,0 +1,11 @@
+package kr.dsm.payedin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PayedInApplication
+
+fun main(args: Array<String>) {
+    runApplication<PayedInApplication>(*args)
+}
---
+++ src/test/kotlin/kr/dsm/payedin/PayedInApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.dsm.payedin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayedInApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
