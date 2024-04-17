98f4dbac4c45f0e474d6c0ff8f4ea259b0754bd6
이하성 <tedsoftj1123@gmail.com>
2024-04-17 12:17:54 +0900

    feat: (#1) base entity 설정

--- src/main/kotlin/kr/dsm/payedin/global/filter/ExceptionFilter.kt
+++ src/main/kotlin/kr/dsm/payedin/global/filter/ExceptionFilter.kt
@@ -7,11 +7,9 @@ import jakarta.servlet.http.HttpServletResponse
 import kr.dsm.payedin.common.exception.CommonException
 import kr.dsm.payedin.global.exception.ExceptionResponse
 import kr.dsm.payedin.global.exception.InternalServerError
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
-@Component
-class GlobalExceptionFilter(
+class ExceptionFilter(
     private val objectMapper: ObjectMapper
 ) : OncePerRequestFilter() {
 
@@ -30,15 +28,16 @@ class GlobalExceptionFilter(
                     logger.warn("${e.cause} on ${request.requestURI}")
                     response.writeExceptionResponse(e.cause as CommonException)
                 }
+
                 else -> {
-                    logger.error(e.stackTrace)
+                    e.printStackTrace()
                     response.writeExceptionResponse(InternalServerError)
                 }
             }
         }
     }
 
-    fun HttpServletResponse.writeExceptionResponse(e: CommonException) {
+    private fun HttpServletResponse.writeExceptionResponse(e: CommonException) {
         this.status = e.status
         this.writer.write(
             objectMapper.writeValueAsString(
