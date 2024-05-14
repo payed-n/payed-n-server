110b3ee5d1673fdcff8e6d5b25d5afa4a1ed62fd
이하성 <tedsoftj1123@gmail.com>
2024-05-14 09:07:30 +0900

    feat: (#main) response status

--- src/main/kotlin/kr/dsm/payedin/domain/wallet/presentation/WalletController.kt
+++ src/main/kotlin/kr/dsm/payedin/domain/wallet/presentation/WalletController.kt
@@ -2,9 +2,11 @@ package kr.dsm.payedin.domain.wallet.presentation
 
 import kr.dsm.payedin.domain.wallet.application.TransferService
 import kr.dsm.payedin.domain.wallet.presentation.dto.TransferRequest
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/wallets")
@@ -13,6 +15,7 @@ class WalletController(
     private val transferService: TransferService
 ) {
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/transfer")
     fun transfer(@RequestBody request: TransferRequest) {
         transferService.execute(request)
